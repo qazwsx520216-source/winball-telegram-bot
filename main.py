60b57fd6153532c928356db3553b3b927f8de6ea
qazwsx520216-source <qazwsx520216@gmail.com>
2025-07-23 17:39:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 async def start(update, context):
     await update.message.reply_text("Hello! 我是 WINBALL AI 預測機器人！")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
