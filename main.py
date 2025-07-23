18511503af65f4d64b1c4278c1a51b8996f6464b
qazwsx520216-source <qazwsx520216@gmail.com>
2025-07-23 17:33:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+from telegram.ext import ApplicationBuilder, CommandHandler
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("WINBALL AI 預測機器人已啟動！")
+async def start(update, context):
+    await update.message.reply_text("Hello! 我是 WINBALL AI 預測機器人！")
 
-if __name__ == "__main__":
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
